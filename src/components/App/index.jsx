714aa36b19fb4bd2ecd07a8dee975ae14ff46fcc
NordOst88 <NordOst88@users.noreply.github.com>
2020-08-03 00:49:15 +0400

    refactor: remove useless files, apply bootstrap and bootswatch themes

---
+++ src/components/App/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../assets/img/logo.png';
+import './style.scss';
+
+function App() {
+  return (
+    <div className="header d-flex">
+        <img src={logo} alt="logo" />
+    </div>
+  );
+}
+
+export default App;
